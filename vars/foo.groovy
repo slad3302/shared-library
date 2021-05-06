02c0f8bcf6f5ed3ff528e97a3e7cdc7eaa6d9c70
slad3302 <76030858+slad3302@users.noreply.github.com>
2021-05-06 17:38:44 +0530

    Update foo.groovy

--- vars/foo.groovy
+++ vars/foo.groovy
@@ -1,3 +1,3 @@
-def call(art){
-  println "Hello Dude, 3 mothoda also working and you version is ${art}"
+def call(version){
+  println "Hello Dude, 3 mothoda also working and you version is ${version}"
 }
