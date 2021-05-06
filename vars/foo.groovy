9ef31a31961a4d9e37c27903ce6fac873961aa58
jenkins <slad3302@gmail.com>
2021-05-06 19:56:58 +0530

    addddd

--- vars/foo.groovy
+++ vars/foo.groovy
@@ -1,7 +1,10 @@
 #!/usr/bin/env groovy
 
+def call(obj1,obj2){
+    println "calling from 1st $obj2"
+    validate(obj1)
+}
 
-def call(obj){
-  println "$obj"
-  println "$game"
+def validate(st){
+    println "calling from 2nd $st"
 }
