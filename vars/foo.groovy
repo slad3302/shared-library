9c22551f5d6f73c785d22e58511c9fcb428698ac
slad3302 <76030858+slad3302@users.noreply.github.com>
2021-05-06 17:53:40 +0530

    Update foo.groovy

--- vars/foo.groovy
+++ vars/foo.groovy
@@ -1,4 +1,6 @@
+#!/usr/bin/env groovy
+
 import org.apache.commons.lang.StringUtils
 def call(obj){
-  println "Hello Dude, 3 mothoda also working and you %obj%"
+  println "Hello Dude, 3 mothoda also working and you $obj"
 }
