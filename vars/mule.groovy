f50966b3d519a3845dbcca32a25c680599f0534b
slad3302 <76030858+slad3302@users.noreply.github.com>
2021-05-07 12:44:53 +0530

    Update mule.groovy

--- vars/mule.groovy
+++ vars/mule.groovy
@@ -1,9 +1,13 @@
 
 
 def foo(obj1, obj2){
-    println "$obj1 $obj2"
+    soo(obj1, obj2)
 }
 
 def coo(obj1, obj2){
     println "$obj2 $obj1"
+}
+
+def soo(st1, st2){
+    println "Hello $st1 and $st2"
 }
