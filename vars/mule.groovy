61b18a8a596fe9d36ad3acedb5992ec00d0a7e77
slad3302 <76030858+slad3302@users.noreply.github.com>
2021-05-07 14:46:09 +0530

    Update mule.groovy

--- vars/mule.groovy
+++ vars/mule.groovy
@@ -1,7 +1,19 @@
 #!/usr/bin/env groovy
 
 def foo(obj1, obj2){
-    return "$obj1 + $obj2"
+    cloud = "$obj1 + $obj2"
+    any_env = su(cloud)
+    any_org = susu(obj1, any_env)
 }
 
+def su(a1){
+    return "hello+$a1"
+}
 
+def susu(q1, q2){
+    return "Namaste $q1 adnd $q2"
+}
+
+def noooo(){
+    println "calling from noooo $any_env"
+}
