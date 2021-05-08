c237ce4e6a9b0aa6da298499a305ee728425ce43
slad3302 <76030858+slad3302@users.noreply.github.com>
2021-05-08 19:53:21 +0530

    Update mule.groovy

--- vars/mule.groovy
+++ vars/mule.groovy
@@ -1,16 +1,46 @@
 #!/usr/bin/env groovy
 
-def generateAppInfo(anypoint_environment, package_name, app_version, tag){
-    if (anypoint_environment == 'Dev'){
-        // package_name = "${params.ARTIFACT_ID}"
-        anypoint_app_name = anypoint_environment.toLowerCase() + "-" + package_name + "-" + app_version
-        build_description = "<b>App name:</b> ${anypoint_app_name},<br> <b>artifactId:</b> ${package_name}"
-    } else {
-        // package_name = (params.TAG_VERSION =~ /(^.*-(proc|sys|exp)-api)/)[ 0 ][ 1 ]
-        version_number = (tag =~ /(\d+\.\d+\.\d+)$/)[ 0 ][ 1 ]
-        anypoint_app_name = anypoint_environment.toLowerCase() + "-" + package_name + "-" + app_version
-        build_description = "<b>App name:</b> ${anypoint_app_name},<br> <b>Tag:</b> ${tag}"
-    }
+def generateAppsInfo(apps, environment, apps_info){
+    apps.split("\\r?\\n").each { line ->
+        def app_info = line.split(',')
 
-    app_properties_path =  "./" + anypoint_environment.toLowerCase() + "/"  + package_name + "-" + app_version + ".json"
+        if (environment == 'Dev'){
+            package_name = "${app_info[0]}"
+            anypoint_app_name = environment.toLowerCase() + "-" + package_name + "-" + app_info[1]
+            version_number = ''
+        } else {
+            package_name = (app_info[0] =~ /(^.*-(proc|sys|exp)-api)/)[ 0 ][ 1 ]
+            version_number = (app_info[0] =~ /(\d+\.\d+\.\d+)$/)[ 0 ][ 1 ]
+            anypoint_app_name = environment.toLowerCase() + "-" + package_name + "-" + app_info[1]
+        }
+
+        app_properties_path =  "./" + environment.toLowerCase() + "/"  + package_name + "-" + app_info[1] + ".json"
+
+        apps_info[app_info[0]] = [
+            app_release_number: app_info[1],
+            app_workers_count: app_info[2],
+            app_workers_type: app_info[3],
+            app_region: app_info[4],
+            package_name: package_name,
+            version_number: version_number,
+            anypoint_app_name: anypoint_app_name,
+            app_properties_path: app_properties_path,
+            rollback: false
+        ]
+    }
 }
+
+def foo(apps_info){
+    apps_info.each{ app, info ->
+            //apps_info[app].current_filename = downloadSnapJar(artifactory_user, artifactory_api_key, apps_info[app].package_name)
+              println apps_info[app].app_release_number
+              println apps_info[app].app_workers_count
+              println apps_info[app].app_workers_type
+              println apps_info[app].app_region
+              println apps_info[app].package_name
+              println apps_info[app].version_number
+			  println apps_info[app].anypoint_app_name 
+              println apps_info[app].app_properties_path
+	}		  
+}
+
