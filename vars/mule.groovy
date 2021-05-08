67943a7123a4060ac688ec4572703ddb5abb5bec
slad3302 <76030858+slad3302@users.noreply.github.com>
2021-05-08 16:00:16 +0530

    Update mule.groovy

--- vars/mule.groovy
+++ vars/mule.groovy
@@ -1,32 +1,16 @@
 #!/usr/bin/env groovy
 
-def generateAppsInfo(apps, environment, apps_info){
-    apps.split("\\r?\\n").each { line ->
-        def app_info = line.split(',')
-
-        if (environment == 'Dev'){
-            package_name = "${app_info[0]}"
-            anypoint_app_name = environment.toLowerCase() + "-" + package_name + "-" + app_info[1]
-            version_number = ''
-        } else {
-            package_name = (app_info[0] =~ /(^.*-(proc|sys|exp)-api)/)[ 0 ][ 1 ]
-            version_number = (app_info[0] =~ /(\d+\.\d+\.\d+)$/)[ 0 ][ 1 ]
-            anypoint_app_name = environment.toLowerCase() + "-" + package_name + "-" + app_info[1]
-        }
-
-        app_properties_path =  "./" + environment.toLowerCase() + "/"  + package_name + "-" + app_info[1] + ".json"
-
-        apps_info[app_info[0]] = [
-            app_release_number: app_info[1],
-            app_workers_count: app_info[2],
-            app_workers_type: app_info[3],
-            app_region: app_info[4],
-            package_name: package_name,
-            version_number: version_number,
-            anypoint_app_name: anypoint_app_name,
-            app_properties_path: app_properties_path,
-            rollback: false
-        ]
+def generateAppInfo(anypoint_environment, package_name, app_version, tag){
+    if (anypoint_environment == 'Dev'){
+        // package_name = "${params.ARTIFACT_ID}"
+        anypoint_app_name = anypoint_environment.toLowerCase() + "-" + package_name + "-" + app_version
+        build_description = "<b>App name:</b> ${anypoint_app_name},<br> <b>artifactId:</b> ${package_name}"
+    } else {
+        // package_name = (params.TAG_VERSION =~ /(^.*-(proc|sys|exp)-api)/)[ 0 ][ 1 ]
+        version_number = (tag =~ /(\d+\.\d+\.\d+)$/)[ 0 ][ 1 ]
+        anypoint_app_name = anypoint_environment.toLowerCase() + "-" + package_name + "-" + app_version
+        build_description = "<b>App name:</b> ${anypoint_app_name},<br> <b>Tag:</b> ${tag}"
     }
-}
 
+    app_properties_path =  "./" + anypoint_environment.toLowerCase() + "/"  + package_name + "-" + app_version + ".json"
+}
