ba5ed8f5d4e22aed6b21c3ff31e17a8b8eaf45be
slad3302 <76030858+slad3302@users.noreply.github.com>
2021-05-10 17:44:36 +0530

    Update mule.groovy

--- vars/mule.groovy
+++ vars/mule.groovy
@@ -1,8 +1,9 @@
 #!/usr/bin/env groovy
 
 def foo(ob1, ob2){
-   return "hello $ob1 and $ob2"
+   prinln "hello $ob1 and $ob2"
 }
 def soo(){
-    println "camecho $user"
+   st1 = "demo"
+   foo(st1)    
 }
